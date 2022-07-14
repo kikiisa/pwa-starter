80c7e16468d1d8d1418804b286c49fb3dde62f36
kikiisa <kikiisa89@gmail.com>
2022-07-14 13:21:03 +0800

    pwa starter

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+const navbar = `
+    <nav class="navbar navbar-dark bg-dark w-1000">
+      <div class="container">
+          <a class="navbar-brand">
+            Mohamad Rizky Isa T3119051
+          </a>
+      </div>
+    </nav>
+    `
+export default { navbar }
